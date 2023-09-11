b739d11846b9b35bd9a877727ee6a12f9dc7817e
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-09-12 03:37:48 +0700

    Update errors.py

--- ArabManage/core/decorators/errors.py
+++ ArabManage/core/decorators/errors.py
@@ -6,7 +6,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from Sriasih import LOG_GROUP_ID, app
+from ArabManage import LOG_GROUP_ID, app
 
 
 def split_limits(text):
